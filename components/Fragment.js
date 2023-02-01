44f315ac385c436c06b64a29518ebd549e1a197c
Touhidulislam622 <106024626+Touhidulislam622@users.noreply.github.com>
2023-02-01 09:00:06 -1200

    Add files via upload

---
+++ components/Fragment.js
@@ -0,0 +1,17 @@
+//  3 way to used ===Fragment====> <> </> 
+
+
+
+//return (   
+{/* <React.Fragment>  
+<h2> Welcome </h2>   
+<p> You are in the world of ReactJS. </p>   
+</React.Fragment>  
+);    */}
+
+
+{/* <React.Fragment>  
+<h2> child1 </h2>   
+<p> child2 </p>   
+      .. ..... .... ...  
+</React.Fragment> */}
